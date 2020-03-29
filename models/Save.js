517a43ad29fbca7fc23ad89dba7f520b3945536e
tinashe <giyavha_tinashe@berkeley.edu>
2020-03-29 04:07:16 -0700

    Fixes models

--- models/Save.js
+++ models/Save.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
-const ArticleSchema = new Schema({
+const SaveSchema = new Schema({
   // `title` is required and of type String
   title: {
     type: String,
@@ -19,11 +19,18 @@ const ArticleSchema = new Schema({
   summary: {
     type: String,
     required: true
+  },
+  // `note` is an object that stores a Note id
+  // The ref property links the ObjectId to the Note model
+  // This allows us to populate the Article with an associated Note
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
   }
 });
 
 // This creates our model from the above schema, using mongoose's model method
-const Article = mongoose.model("Article", ArticleSchema);
+const Save = mongoose.model("Saved", SaveSchema);
 
 // Export the Article model
-module.exports = Article;
+module.exports = Save;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-  Article: require("./Article"),
-  Note: require("./Note")
-};
+    Article: require("./Article"),
+    Save: require("./Save"),
+    Note: require("./Note")
+  };
+  
